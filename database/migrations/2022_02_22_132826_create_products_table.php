7e5cc1eb0d126ae73427f8a79641aa5fe8d528c3
Jose Victor Persset <jpersset@gmail.com>
2022-02-22 10:35:22 -0300

    Setup create products table migration

---
+++ database/migrations/2022_02_22_132826_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->integer('weight')->nullable();
+            $table->decimal('sell_price', 10, 2)->default(1.00);
+            $table->integer('max_stock')->default(1);
+            $table->integer('min_stock')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
