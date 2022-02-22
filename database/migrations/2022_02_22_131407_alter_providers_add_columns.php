726f0d3485a2d04949b52586030e21c137c63c43
Jose Victor Persset <jpersset@gmail.com>
2022-02-22 10:20:44 -0300

    Setup migration to add columns to table providers

---
+++ database/migrations/2022_02_22_131407_alter_providers_add_columns.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('providers', function(Blueprint $table) {
+            $table->string('uf', 2);
+            $table->string('email', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('providers', function (Blueprint $table) {
+            /*$table->dropColumn('uf');
+            $table->dropColumn('email');*/
+            $table->dropColumn(['uf', 'email']);
+        });
+    }
+};
