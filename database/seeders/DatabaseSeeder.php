eb24a385b419b5d65fc391c3a4e6d22daa20bcaa
Jose Victor Persset <jpersset@gmail.com>
2022-02-25 17:16:46 -0300

    Setup Site_contacts table db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         
         $this->call(ProviderSeeder::class);
+        $this->call(SiteContactSeeder::class);
     }
 }
--- database/seeders/SiteContactSeeder.php
+++ database/seeders/SiteContactSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\SiteContact;
 
 class SiteContactSeeder extends Seeder
 {
@@ -14,6 +15,12 @@ class SiteContactSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $contact = new SiteContact();
+        $contact->name = 'Sistema SG';
+        $contact->phone = '(11) 91234-5678';
+        $contact->email = 'contato@sg.com.br';
+        $contact->contact_subject = '1';
+        $contact->message = 'Bem-vindo ao sistema Super Gestão';
+        $contact->save();
     }
 }
