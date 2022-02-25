ddbb75a20ef1f17ce05fe171ad85710d03fd8011
Jose Victor Persset <jpersset@gmail.com>
2022-02-25 16:43:34 -0300

    Tinker knowledge

--- app/Models/SiteContact.php
+++ app/Models/SiteContact.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SiteContact extends Model
-{
+class SiteContact extends Model {
     use HasFactory;
 }
