4ce79a423ae2748839c3b9f91bb6a6f13ecc926e
Jose Victor Persset <jpersset@gmail.com>
2022-03-02 17:02:07 -0300

    Add basic error messages to field validations on contact form

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -17,13 +17,28 @@ class ContactController extends Controller
     }
 
     public function saveContact(Request $request) {
-        $request->validate([
+
+        $validationRules = [
             'name' => 'required|min:2|max:140',
             'phone' => 'required',
             'email' => 'email',
             'contact_subject_id' => 'required',
             'message' => 'required|max:2000'
-        ]);
+        ];
+
+        $validationErrorMessages = [
+            'name.required' => 'O campo nome é obrigatório',
+            'name.min' => 'O campo nome precisa ter no mínimo 2 caracteres',
+            'name.max' => 'O campo nome deve ter no máximo 140 caracteres',
+
+            'email' => 'Por favor insira um endereço de email válido',
+
+            'message.max' => 'O campo mensagem deve ter no máximo 2000 caracteres',
+
+            'required' => 'O campo é obrigatório',
+        ];
+
+        $request->validate($validationRules, $validationErrorMessages);
 
         SiteContact::create($request->all());
         return redirect()->route('site.index');
--- resources/views/site/layouts/_components/contactForm.blade.php
+++ resources/views/site/layouts/_components/contactForm.blade.php
@@ -3,26 +3,33 @@
 <form action={{ route('site.contact') }} method="post">
     @csrf
     <input name="name" type="text" placeholder="Nome" class="{{$class}}" value="{{old('name')}}"">
+    @if ($errors->has('name'))
+        {{ $errors->first('name') }}
+    @endif
     <br>
+
     <input name=" phone" type="text" placeholder="Telefone" class="{{$class}}" value="{{old('phone')}}"">
+    {{$errors->has('phone') ? $errors->first('phone') : ''}}
     <br>
+
     <input name=" email" type="email" placeholder="E-mail" class="{{$class}}" value="{{old('email')}}"">
+    {{$errors->has('email') ? $errors->first('email') : ''}}
     <br>
+
     <select name=" contact_subject_id" class="{{$class}}">
     <option value="">Qual o motivo do contato?</option>
     @foreach($contact_subjects as $key => $contact_subject)
-    <option value="{{$contact_subject->id}}" {{ old('contact_subjects_id')==$contact_subject->id ? 'selected' : ''
+    <option value="{{$contact_subject->id}}" {{ old('contact_subject_id')==$contact_subject->id ? 'selected' : ''
         }}>{{$contact_subject->subject}}</option>
     @endforeach
     </select>
+    {{$errors->has('contact_subject_id') ? $errors->first('contact_subject_id') : ''}}
     <br>
+
     <textarea name="message" class="{{$class}}"
         placeholder="Preencha aqui a sua mensagem">{{ (old('message') != '') ? old('message') : '' }}</textarea>
+    {{$errors->has('message') ? $errors->first('message') : ''}}
     <br>
+
     <button type="submit" class="{{$class}}">ENVIAR</button>
 </form>
-<div style="position:absolute; top:0px; width:100%; background:red">
-    <pre>
-    {{ print_r($errors) }}
-    </pre>
-</div>
